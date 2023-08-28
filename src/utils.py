0c5ad0f9ad017ffdefae391c4ee0272d183af4a9
kien <akitak1290@gmail.com>
2023-08-27 22:45:16 -0400

    refactor: move type def to util

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import os
 from pathlib import Path
+from typing import TypedDict, Any
+import customtkinter
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 ASSETS_DIR = os.path.join(Path(__file__).absolute().parent.parent, 'assets')
+
+class AssetT(TypedDict):
+	file_path: str
+	file_name: str
+	file_format: str
+	image: customtkinter.CTkImage
+	widget: customtkinter.CTkButton
